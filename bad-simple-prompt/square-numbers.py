76a0bdb6aa9658c07b2f5c11a8465ffe2958b7b5
Gutemberg Borges <gutem.borges@gmail.com>
2026-06-23 13:39:13 -0300

    Initial commit: Initialize with bad prompt example to square-number function

---
+++ bad-simple-prompt/square-numbers.py
@@ -0,0 +1,13 @@
+# bad and simple prompt: 
+# "write a function that will square number in a list
+# ? What a language? Any negative number? Any non-numbers? Return a new list?
+
+def square_numbers(numbers):
+    for i in range(len(numbers)):
+        numbers[i] = numbers[i] ** 2
+    return numbers
+
+# Usage
+nums = [1, 2, 3, 4, 5]
+result = square_numbers(nums)
+print(result)  # Output: [1, 4, 9, 16, 25]
